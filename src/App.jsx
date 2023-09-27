f79fb704a29e2a4c66b3da16c5712e6eb7dfb2e8
Camilo7896 <cg082490@gmail.com>
2023-09-26 22:32:57 -0500

    Se modifico el texto

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,18 @@ function App() {
 
   return (
     <>
-     <h1>Estamos en antenimiento</h1>
+     <h1>Mentha Ecos</h1>
      <img className='logo' src={Logo} alt='logo'/>
+     <p>Lamentablemente, estamos realizando mejoras en nuestro sitio web para brindarte una experiencia aún mejor. Estamos trabajando arduamente para asegurarnos de que Mentha Ecos esté en su mejor forma y esté listo para ofrecerte contenido fresco y emocionante.
+
+Durante este período de mantenimiento, aún puedes mantenerte al tanto de nuestras actualizaciones y noticias en nuestras redes sociales. No dudes en seguirnos en Facebook, Instagram y Twitter para obtener las últimas novedades y consejos sobre sostenibilidad.
+
+Estamos ansiosos por volver a verte en línea pronto. Mientras tanto, si tienes alguna pregunta o necesitas ponerse en contacto con nosotros, no dudes en enviar un correo electrónico a [tu dirección de correo electrónico] y estaremos encantados de ayudarte.
+
+Gracias por tu paciencia y comprensión mientras trabajamos para mejorar tu experiencia en Mentha Ecos. ¡Esperamos verte pronto!
+
+Atentamente,
+El equipo de Mentha Ecos</p>
     </>
   )
 }
